a8c2566e997434b69c8f9939195a3f0ea4a2e541
Wu Julie <julie-wu@hotmail.com>
2024-05-23 09:50:50 +0200

    BUG: fin bug responsivenes

--- src/views/smartwatch/HearthRate.js
+++ src/views/smartwatch/HearthRate.js
@@ -71,7 +71,7 @@ const HearthRate = () => {
             alignItems: 'center',
             justifyContent: 'center',
             position: 'relative',
-            width: '100%', // Makes the Box responsive
+            
           }}
         >
           {/* Ajout du titre */}
@@ -83,10 +83,30 @@ const HearthRate = () => {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            width: '25vw',
-            height: '25vw',
+            width:  '25vh',
+            height: '25vh',
           }}>
-            <img src={heartImageUrl} alt="Heart" style={{ width: '100%', height: '100%' }} />
+            {/*changer l'image coeur avec svg coeur*/}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -1 34 34" version="1.1">
+    <title>heart</title>
+    <desc>Created with Sketch.</desc>
+    <defs>
+
+</defs>
+    <g id="Vivid.JS" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Vivid-Icons" transform="translate(-824.000000, -411.000000)" fill="#FF6E6E">
+            <g id="Icons" transform="translate(37.000000, 169.000000)">
+                <g id="heart" transform="translate(780.000000, 234.000000)">
+                    <g transform="translate(7.000000, 8.000000)" id="Shape">
+                        <path d="M17,32 C17,32 0,27.578 0,10.482 C0,5.245 4.349,8.8817842e-16 9.714,8.8817842e-16 C12.5330763,0.114312803 15.1754592,1.40395232 17,3.556 C18.8245408,1.40395232 21.4669237,0.114312803 24.286,0 C29.651,8.8817842e-16 34,5.245 34,10.482 C34,27.523 17,32 17,32 Z">
+
+</path>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
             <h1 style={{
               position: 'absolute',
               color: 'white',
@@ -107,8 +127,8 @@ const HearthRate = () => {
             alignItems: 'center',
             justifyContent: 'center',
             position: 'relative',
-            width: '100%', // Makes the Box responsive
-
+            width: '100%',
+            height: '40vh',
           }}>
             <Line data={data} options={{ responsive: true, maintainAspectRatio: false }} />
           </Box>
